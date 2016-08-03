8e7b7922d478727c3632ec2e5f5627ed3899b9bf
Camilina <castegong@gmail.com>
2016-08-03 05:36:47 -0400

    scrolling and menu fixed, icon rrss, and footer

--- js/main.js
+++ js/main.js
@@ -3,3 +3,21 @@ $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 	$("icon-me").click();
 });
+
+//menu fijo
+$(document).ready(function(){
+	$(window).bind('scroll', function() {
+		var navHeight = $( window ).height() - 70;
+		if ($(window).scrollTop() > navHeight) {
+			$('nav').addClass('fixed');
+		}
+		else {
+			$('nav').removeClass('fixed');
+		}
+	});
+});
+
+//scroller
+$(document).ready(function() {
+	$("a.scroller").smoothscrolling();
+});
